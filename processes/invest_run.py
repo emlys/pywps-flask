e040d3f15a81eced5bf5b58ec965786f3f60b478
Emily Soth <esoth@stanford.edu>
2025-06-17 15:49:41 -0700

    fix some small bugs and typos

--- processes/invest_run.py
+++ processes/invest_run.py
@@ -7,17 +7,19 @@ import types
 import natcap.invest
 from natcap.invest import models
 from natcap.invest import spec
-from pywps import Process, ComplexInput, LiteralOutput, Format, UOM
-
+import pywps
+from pywps import Process, ComplexInput, LiteralInput, ComplexOutput, UOM
+from pywps.validator.mode import MODE
+from pywps.inout.formats import FORMATS
 
 def generate_model_inputs(MODEL_SPEC):
     inputs = []  # model inputs may be files or literal types
-    for model_input in module.MODEL_SPEC.inputs:
+    for model_input in MODEL_SPEC.inputs:
 
         min_occurs = 1 if model_input.required is True else 0
         input_kwargs = dict(
             identifier=model_input.id,
-            title=model_input.title,
+            title=model_input.name,
             abstract=model_input.about,
             workdir=None,  # workspace dir?
             min_occurs=min_occurs,
@@ -28,13 +30,16 @@ def generate_model_inputs(MODEL_SPEC):
         if (isinstance(model_input, spec.FileInput) or
                 isinstance(model_input, spec.DirectoryInput)):
             if isinstance(model_input, spec.VectorInput):
-                supported_formats = [Formats.GEOJSON]
+                supported_formats = [FORMATS.GEOJSON]
             elif type(model_input) in {spec.SingleBandRasterInput, spec.RasterInput}:
-                supported_formats = [Formats.GEOTIFF]
+                supported_formats = [FORMATS.GEOTIFF]
             elif isinstance(model_input, spec.CSVInput):
-                supported_formats = [Formats.CSV]
+                supported_formats = [FORMATS.CSV]
             elif isinstance(model_input, spec.RasterOrVectorInput):
-                supported_formats = [Formats.GEOTIFF, Formats.GEOJSON]
+                supported_formats = [FORMATS.GEOTIFF, FORMATS.GEOJSON]
+            elif isinstance(model_input, spec.DirectoryInput):
+                supported_formats = []  # what to do here?
+
             # Unclear what to do in case of unspecified file type,
             # but this is not used by any invest model.
             # elif isinstance(model_input, spec.FileInput):
@@ -47,7 +52,7 @@ def generate_model_inputs(MODEL_SPEC):
             data_type = {
                 spec.NumberInput: 'float',
                 spec.PercentInput: 'float',
-                spec.RatioInput: 'float'
+                spec.RatioInput: 'float',
                 spec.IntegerInput: 'integer',
                 spec.StringInput: 'string',
                 spec.OptionStringInput: 'string',
@@ -72,15 +77,19 @@ def generate_model_inputs(MODEL_SPEC):
 
 def generate_model_outputs(MODEL_SPEC):
     outputs = []
-    for model_output in module.MODEL_SPEC.outputs:
-        if isinstance(model_input, spec.VectorInput):
-            data_format = [Formats.GEOJSON]
-        elif type(model_input) in {spec.SingleBandRasterInput, spec.RasterInput}:
-            data_format = [Formats.GEOTIFF]
-        elif isinstance(model_input, spec.CSVInput):
-            data_format = [Formats.CSV]
-        elif isinstance(model_input, spec.FileOutput):
-            data_format = [Formats.HTML]
+    for model_output in MODEL_SPEC.outputs:
+        if isinstance(model_output, spec.VectorOutput):
+            data_format = FORMATS.GEOJSON
+        elif type(model_output) in {spec.SingleBandRasterOutput, spec.RasterOutput}:
+            data_format = FORMATS.GEOTIFF
+        elif isinstance(model_output, spec.CSVOutput):
+            data_format = FORMATS.CSV
+        elif isinstance(model_output, spec.FileOutput):
+            data_format = None # FORMATS.HTML todo?
+        elif isinstance(model_output, spec.DirectoryOutput):
+            data_format = None  # todo?
+        else:
+            print(model_output)
         outputs.append(ComplexOutput(  # all model outputs are files
             identifier=model_output.id,
             title=model_output.id,
@@ -91,9 +100,9 @@ def generate_model_outputs(MODEL_SPEC):
 
 def get_model_processes():
     model_processes = []
-    for model_id, module in models.model_id_to_module:
+    for module in models.pyname_to_module.values():
 
-        def handler(self, request: pywps.app.WPSRequest, response: pywps.app.WPSResponse):
+        def handler(self, request: pywps.app.WPSRequest, response: pywps.response.basic.WPSResponse):
 
             # parse request.inputs into args dict
             args = request.inputs
@@ -102,20 +111,25 @@ def get_model_processes():
 
             # set response values
 
+        # dynamically make a class inherited from pywps.Process
+        # to represent each invest model
+        new_class = type(
+            f'ExecuteInVEST_${module.MODEL_SPEC.model_id}',
+            (Process,),
+            {}
+        )
 
-        def init(self):
-            super().__init__(
+        def __init__(self):
+            print('__init__!')
+            super(new_class, self).__init__(
                 handler=handler,
                 identifier=module.MODEL_SPEC.model_id,
                 title=module.MODEL_SPEC.model_title,
                 inputs=generate_model_inputs(module.MODEL_SPEC),
                 outputs=generate_model_outputs(module.MODEL_SPEC))
 
-        # dynamically make a class inherited from pywps.Process
-        # to represent each invest model
-        model_processes.append(type(
-            name=f'ExecuteInVEST_${module.MODEL_SPEC.model_id}',
-            bases=(Process,),
-            attrs=dict(__init__=init)))
+
+        new_class.__init__ = __init__
+        model_processes.append(new_class())
 
     return model_processes
