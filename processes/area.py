167c30ad7eb9ba471ea4a59bb8b6d1cc7213d4b3
Emily Soth <esoth@stanford.edu>
2025-06-17 10:24:37 -0700

    remove reference to pywps module that no longer exists

--- processes/area.py
+++ processes/area.py
@@ -1,9 +1,9 @@
-
 import json
 import os
 import subprocess
+import tempfile
+
 from pywps import Process, ComplexInput, LiteralOutput, Format
-from pywps.wpsserver import temp_dir
 
 __author__ = 'matteo'
 
@@ -31,16 +31,22 @@ class Area(Process):
     def _handler(self, request, response):
         # ogr2ogr requires gdal-bin
         from shapely.geometry import shape
-        with temp_dir() as tmp:
+
+        try:
+            tmp = tempfile.mkdtemp()
+
             input_gml = request.inputs['layer'][0].file
             input_geojson = os.path.join(tmp, 'input.geojson')
             subprocess.check_call(['ogr2ogr', '-f', 'geojson',
                                    str(input_geojson), input_gml])
             with open(input_geojson, 'rb') as f:
                 data = json.loads(f.read())
-            for feature in data['features']:
-                geom = shape(feature['geometry'])
-                feature['area'] = geom.area
-            response.outputs['area'].data = [feature['area']
-                                             for feature in data['features']]
-            return response
+        finally:
+            shutil.rmtree(tmp)
+
+        for feature in data['features']:
+            geom = shape(feature['geometry'])
+            feature['area'] = geom.area
+        response.outputs['area'].data = [feature['area']
+                                         for feature in data['features']]
+        return response
